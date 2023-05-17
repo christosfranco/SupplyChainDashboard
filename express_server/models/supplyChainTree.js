e09708b6d4f12d407ba00ba7ce641131a7bf9062
christosfranco <fpk278@alumni.ku.dk>
2023-05-17 18:24:48 +0200

    made parser, still has bugs

--- express_server/models/supplyChainTree.js
+++ express_server/models/supplyChainTree.js
@@ -1,5 +1,28 @@
-const SupplyChainTree = {
-  id: '',
-  name: ''
-  // etc... you see the pattern
+module.exports = {
+  Node,
+  Risk,
+  Data,
+};
+
+function Node() {
+  this.Node_ID = 0;
+  this.Root = false;
+  this.Node_name = "";
+  this.Type = "";
+  this.Supplying_To = [];
+  this.Suppliers = [];
+  this.Risks = [];
+}
+
+function Risk() {
+  this.Name = "";
+  this.Risk_ID = 0;
+  this.Consequence = 0;
+  this.Likelihood = 0;
+  this.Mitigation_Strategies = [];
+  this.Concern_IDs = [];
+}
+
+function Data() {
+  this.Nodes = [];
 }
